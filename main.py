47335b8d38b42c37495142d8853c2145f3fd85da
jiaxi312 <43364400+jiaxi312@users.noreply.github.com>
2022-11-14 22:40:24 -0600

    Add properties and functions to compute Processor clock.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from processor import Processor
+import datetime
+import time
 
 
 def main():
-    print(Processor.NORMAL)
+    p = Processor(1, 100)
+    now = datetime.datetime.now()
+    print(now)
+    print(p.clock)
+    print(p)
 
 
 if __name__ == '__main__':
--- processor.py
+++ processor.py
@@ -1,26 +1,41 @@
+import random
+import time
+import datetime
+
+
 class Processor:
     """A class represents a single Processor in the membership system
 
     A Processor represents the abstract concept of Processor or Server in Cristian’s
     "Reaching Agreement on Processor Group Membership in Synchronous Distributed Systems" paper.
     A Processor can send a direct message to other processor or broadcast to all correct processors. It also keeps track
-    of its status, its view of the membership of group T
+    of its status, its view of the membership of group T. It may also contain clock synchronization error when
+    initialized.
 
     Attributes:
+        _id: A int indicating the id of this Processor
         _current_group: A int indicating the current group the processor belongs to
         _status: A int constant indicating the status of the Processor
         _membership: A list contains the Processor's view of its memberships in the _current_group
+        _clock_diff: A float representing the clock synchronization error of this Processor clock from the master clock
     """
 
     NORMAL = 1
     CRASHED = -1
 
-    def __init__(self):
+    def __init__(self, id, max_clock_sync_error):
+        """ Inits the Processor with given max clock synchronization error """
+        self._id = id
         self._current_group = None
         self._status = Processor.NORMAL
         self._membership = []
+        self._clock_diff = (random.random() - 0.5) * max_clock_sync_error
 
     """Class properties"""
+    @property
+    def id(self):
+        return self._id
+
     @property
     def group(self):
         return self._current_group
@@ -37,3 +52,16 @@ class Processor:
     @status.setter
     def status(self, new_status):
         self._status = new_status
+
+    @property
+    def clock(self):
+        """Returns the clock reading of this Processor
+
+        The reading is computed as H(t)+A where H(t) is the standard current time
+        """
+        now = time.time()
+        tz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+        return datetime.datetime.fromtimestamp(now + self._clock_diff, tz=tz)
+
+    def __str__(self):
+        return f'Processor {self.id}, group: {self.group}, clock diff: {self._clock_diff}'
